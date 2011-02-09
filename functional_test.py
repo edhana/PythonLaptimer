2a2c92f893099e16510001f2407d284eb2a281ac
edhana <edhana@gmail.com>
2011-02-09 14:22:33 -0200

    Developing the first message validation tests

--- functional_test.py
+++ functional_test.py
@@ -1,16 +1,56 @@
 import unittest
 from mockito import *
+import os
 
-import src.GPS # Keep's Consistency with the REAL spec for this module
+from src import GPS # Keep Consistency with the REAL spec for this module
+from src import main
 
 class TestFunctional (unittest.TestCase):
     def setUp(self):
-        pass
-        # self.serialInterface = SerialInterface('karmonitor.log')
-        # self.gprs = GPRS(self.serialInterface, GPRS.OPERATOR_VIVO)
-        # self.kClient = MessageFactory('KAR', 1234, 4)
-        # when(PropertiesLoader).get_splitted_plate().thenReturn(['TTT', 1111])
-        # when(GPS).getActualPosition().thenReturn('225109.000,1545.1554S,04752.9177W,0.6,1066.3,3,190.05,0.03,0.01,261010,10')
+        self.log_file = open(main.GPS_LOG_FILE_NAME, 'w')
+        when(GPS).getActualPosition().thenReturn('225109.000,1545.1554S,04752.9177W,0.6,1066.3,3,190.05,0.03,0.01,261010,10')
+        
+    def tearDown(self):
+        if self.log_file != None:
+            # logfile = None
 
-    def test_should_send_a_message_when_everything_goes_right(self):
-        pass
+            try:
+                # logfile = open(main.GPS_LOG_FILE_NAME, 'r')
+            
+                # Delete the file
+                if self.log_file != None:
+                    self.log_file.close()
+                    os.remove(main.GPS_LOG_FILE_NAME)
+                
+            except IOError:
+                print "\nThe file %s was not found"%main.GPS_LOG_FILE_NAME
+                
+    def test_sould_return_true_when_a_valid_message_is_passed(self):
+        invalid_message = '225109.000,1545.1554S,04752.9177W,0.6,1066.3,3,190.05,0.03,0.01,261010,10'
+        spl_message = invalid_message.split(',')
+
+        self.assertTrue(main.is_valid_message(spl_message))
+                    
+    def test_sould_return_false_when_an_invalid_message_is_passed(self):
+        invalid_message = '235948.043,,,,,0,,,,140209,00'
+        spl_message = invalid_message.split(',')
+        
+        self.assertFalse(main.is_valid_message(spl_message))
+        
+        
+    # def test_should_get_formated_gps_data(self):
+    #     string_to_write = "Testing"
+    #     readed_string = ""
+    #     
+    #     formated_data = main.get_formated_gps_data()
+    #     self.assertTrue(formated_data != "")
+    #     
+        # self.assertEquals(readed_string, string_to_write)
+
+if __name__ == '__main__':
+    # unique = unittest.TestSuite()
+    #     unique.addTest(TestFunctional('test_should_send_a_start_message'))
+    #     unittest.TextTestRunner().run(unique)
+    unittest.main()
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,23 +1,43 @@
 import SER
 import GPS
 
-SPEED_LOWER_LIMIT=5 # 5km de limite minimo para registrar
-
-def open_log_file():
-    pass
-    
-def close_log_file():
-    pass
+SPEED_LOWER_LIMIT=5 # Minimum speed limit to save gps data
+GPS_LOG_FILE_NAME="input.log"
     
 def write_on_file(file, gps_data):
     pass
     
+def is_valid_message(gps_split_message):
+    # verify the fixmode at the 6th element of array
+    if (int(gps_split_message[5]) == 3):
+        return 1
+    else:
+        return 0
+    
 def get_formated_gps_data():
-    GPS.getActualPosition()
+    gps_string = GPS.getActualPosition()
+    
+    gps_split_message = gps_string.split(',')
+    
+    return_message = ""
+    
+    return  return_message# TODO: Implementar
 
+'''
+    Basic loop function of this app.
+'''
+def loop():
+    # Open de log file
+    gps_log_file = None
+    
+    gps_log_file = open(GPS_LOG_FILE_NAME, 'w')
+    
+    # Dont forget to close the log file
+    gps_log_file.close()
+    
 if __name__ == "__main__":
     SER.set_speed("115200")
     SER.send("Iniciando cliente Lap Timer")
     
-    # Abre o arquivo onde serão colocados as coordenadas
-    open_log_file()
+    while(1):
+        loop()
